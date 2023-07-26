dc9670986f48b556ef536d097b84946660a25411
artemflood <artemflood@gmail.com>
2023-07-26 16:38:26 +0300

    Init repository

---
+++ src/moks/index.ts
@@ -0,0 +1,272 @@
+export const users = [
+    {
+        "_id": "64b02501eac93cd27e8ac063",
+        "index": 0,
+        "guid": "7be43682-1e4f-40b0-bc3a-0962ef651846",
+        "isActive": false,
+        "balance": "$1,980.96",
+        "picture": "http://placehold.it/32x32",
+        "age": 40,
+        "eyeColor": "green",
+        "name": "Velasquez Mcdowell",
+        "gender": "male",
+        "company": "EARWAX",
+        "email": "velasquezmcdowell@earwax.com",
+        "phone": "+1 (859) 461-2296",
+        "address": "912 Carroll Street, Biehle, Pennsylvania, 8478",
+        "about": "Fugiat eu consequat consequat labore. Laboris consequat amet eu sit veniam velit culpa do id ea magna dolore id. Ipsum commodo tempor qui nostrud laborum reprehenderit. Tempor esse anim dolor aute pariatur cillum occaecat ullamco. Exercitation magna enim proident do ea fugiat cupidatat labore aliqua labore fugiat sint anim amet.\r\n",
+        "registered": "2020-05-01T08:48:08 -03:00",
+        "latitude": 58.271644,
+        "longitude": 19.700287,
+        "tags": [
+            "laborum",
+            "non",
+            "mollit",
+            "ad",
+            "mollit",
+            "voluptate",
+            "eu"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Hubbard Nguyen"
+            },
+            {
+                "id": 1,
+                "name": "Stanley Norris"
+            },
+            {
+                "id": 2,
+                "name": "Amparo Ramsey"
+            }
+        ],
+        "greeting": "Hello, Velasquez Mcdowell! You have 4 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "64b02501fb68215ec880ad37",
+        "index": 1,
+        "guid": "070d71e3-d683-47d6-a148-28bbe7d263ae",
+        "isActive": true,
+        "balance": "$1,687.80",
+        "picture": "http://placehold.it/32x32",
+        "age": 37,
+        "eyeColor": "blue",
+        "name": "Vargas Thornton",
+        "gender": "male",
+        "company": "INTERLOO",
+        "email": "vargasthornton@interloo.com",
+        "phone": "+1 (904) 531-2585",
+        "address": "803 Hopkins Street, Orick, Montana, 3972",
+        "about": "Ut culpa sint irure consequat laborum duis velit eiusmod. Sunt sint exercitation ullamco dolor nisi ut exercitation officia in proident ut. Enim ipsum mollit minim id ipsum laboris anim sunt consequat.\r\n",
+        "registered": "2020-09-05T08:45:17 -03:00",
+        "latitude": -63.649127,
+        "longitude": 158.873566,
+        "tags": [
+            "ut",
+            "enim",
+            "in",
+            "do",
+            "consequat",
+            "adipisicing",
+            "laboris"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Bailey Jacobs"
+            },
+            {
+                "id": 1,
+                "name": "Wyatt Floyd"
+            },
+            {
+                "id": 2,
+                "name": "Selma Combs"
+            }
+        ],
+        "greeting": "Hello, Vargas Thornton! You have 8 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "64b02501bc52ed8907faa4e9",
+        "index": 2,
+        "guid": "0b0396ec-5e8d-4d40-abec-1c9a4d33897a",
+        "isActive": false,
+        "balance": "$1,377.20",
+        "picture": "http://placehold.it/32x32",
+        "age": 27,
+        "eyeColor": "blue",
+        "name": "Gamble Maddox",
+        "gender": "male",
+        "company": "GAPTEC",
+        "email": "gamblemaddox@gaptec.com",
+        "phone": "+1 (849) 488-2133",
+        "address": "840 Channel Avenue, Glidden, Massachusetts, 8964",
+        "about": "Ea ullamco cillum pariatur sint occaecat. Esse ad pariatur nisi aliqua dolore in officia duis deserunt. Velit aliqua do ipsum non officia sint duis voluptate ullamco. Do laboris sit esse in. Labore ea aliquip est laboris id nisi exercitation mollit duis proident culpa occaecat. Est voluptate ea id aliqua labore et aute officia anim officia ipsum est aute fugiat.\r\n",
+        "registered": "2021-10-11T09:53:47 -03:00",
+        "latitude": -85.766817,
+        "longitude": 166.472789,
+        "tags": [
+            "ipsum",
+            "velit",
+            "nisi",
+            "in",
+            "eiusmod",
+            "est",
+            "minim"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Deanna Cook"
+            },
+            {
+                "id": 1,
+                "name": "Merritt Booker"
+            },
+            {
+                "id": 2,
+                "name": "Kramer Clemons"
+            }
+        ],
+        "greeting": "Hello, Gamble Maddox! You have 8 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "64b02501cb9078d0dfd0c560",
+        "index": 3,
+        "guid": "b5c23666-c55b-4cd0-abb2-e619e6c02c9c",
+        "isActive": true,
+        "balance": "$3,356.62",
+        "picture": "http://placehold.it/32x32",
+        "age": 40,
+        "eyeColor": "blue",
+        "name": "Huber Stephens",
+        "gender": "male",
+        "company": "JOVIOLD",
+        "email": "huberstephens@joviold.com",
+        "phone": "+1 (904) 428-3183",
+        "address": "802 Beach Place, Saddlebrooke, Mississippi, 750",
+        "about": "Ex aliqua ullamco esse dolor dolore culpa pariatur occaecat nisi aute cupidatat quis qui. Eu minim cupidatat labore et laboris enim et. Duis proident cupidatat nostrud ullamco et enim cillum velit ea eiusmod eu sit. Aliquip incididunt eu eu sunt aliquip tempor magna aliqua dolore irure do eiusmod reprehenderit adipisicing. Nisi culpa dolor adipisicing aliquip sunt aliquip ad pariatur aliquip.\r\n",
+        "registered": "2017-12-04T07:36:20 -02:00",
+        "latitude": -76.360595,
+        "longitude": -130.257669,
+        "tags": [
+            "do",
+            "officia",
+            "enim",
+            "elit",
+            "velit",
+            "Lorem",
+            "enim"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Dorothea Murphy"
+            },
+            {
+                "id": 1,
+                "name": "Isabella Coffey"
+            },
+            {
+                "id": 2,
+                "name": "Olson Pennington"
+            }
+        ],
+        "greeting": "Hello, Huber Stephens! You have 9 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "64b025010967a2982d61d162",
+        "index": 4,
+        "guid": "29c9f38b-cf81-4cb3-8df3-ad162857cc59",
+        "isActive": true,
+        "balance": "$3,420.86",
+        "picture": "http://placehold.it/32x32",
+        "age": 26,
+        "eyeColor": "blue",
+        "name": "Harrell Stout",
+        "gender": "male",
+        "company": "INTERODEO",
+        "email": "harrellstout@interodeo.com",
+        "phone": "+1 (877) 491-2760",
+        "address": "289 Jodie Court, Bend, Michigan, 9579",
+        "about": "Ea labore adipisicing cillum amet veniam. Occaecat aliquip laboris tempor ea laboris commodo sunt nulla cillum sunt. Reprehenderit commodo et amet consectetur proident nulla eu nisi ea incididunt irure excepteur non. Lorem nisi qui velit irure.\r\n",
+        "registered": "2015-02-02T01:54:37 -02:00",
+        "latitude": 48.411369,
+        "longitude": -58.443552,
+        "tags": [
+            "nisi",
+            "consequat",
+            "consectetur",
+            "enim",
+            "ut",
+            "consectetur",
+            "exercitation"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Martinez Beasley"
+            },
+            {
+                "id": 1,
+                "name": "Keri Ortega"
+            },
+            {
+                "id": 2,
+                "name": "Maribel Brown"
+            }
+        ],
+        "greeting": "Hello, Harrell Stout! You have 4 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "64b025012522df86a7caab46",
+        "index": 5,
+        "guid": "33b2b1ff-d367-461b-a959-35c8a435ce4a",
+        "isActive": true,
+        "balance": "$3,830.70",
+        "picture": "http://placehold.it/32x32",
+        "age": 20,
+        "eyeColor": "blue",
+        "name": "Anne Evans",
+        "gender": "female",
+        "company": "UTARIAN",
+        "email": "anneevans@utarian.com",
+        "phone": "+1 (881) 425-3646",
+        "address": "284 Gallatin Place, Lindisfarne, Minnesota, 4805",
+        "about": "Laboris veniam culpa magna eu mollit et voluptate. Cillum aute tempor reprehenderit ullamco labore in. Anim amet ipsum aliquip velit dolor enim et. Officia pariatur aute nisi et incididunt esse do. Irure cillum et duis laboris mollit aute sunt velit esse eu tempor consequat. Ut amet velit do et dolore mollit.\r\n",
+        "registered": "2018-08-16T11:38:59 -03:00",
+        "latitude": 56.248737,
+        "longitude": 177.723526,
+        "tags": [
+            "ut",
+            "sunt",
+            "occaecat",
+            "fugiat",
+            "excepteur",
+            "minim",
+            "quis"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Clark Sandoval"
+            },
+            {
+                "id": 1,
+                "name": "Lorna Donaldson"
+            },
+            {
+                "id": 2,
+                "name": "Hess Wiggins"
+            }
+        ],
+        "greeting": "Hello, Anne Evans! You have 4 unread messages.",
+        "favoriteFruit": "banana"
+    }
+]
