1e18afbd99c89c1d8a0a1425c7ac764b8bcc52ac
artemflood <artemflood@gmail.com>
2023-08-12 20:03:39 +0300

    Refactoring app - Improved auth service - Improved watchlist service - Improved user service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -12,22 +12,30 @@ export class AuthService {
                 private readonly tokenService: TokenService) {
     }
     async  registerUsers(dto: CreateUserDTO): Promise<CreateUserDTO>{
-        const existUser = await this.userService.findUserByEmail(dto.email)
-        if (existUser) throw new BadRequestException(AppError.USER_EXIST)
-        return this.userService.createUser(dto)
+       try {
+           const existUser = await this.userService.findUserByEmail(dto.email)
+           if (existUser) throw new BadRequestException(AppError.USER_EXIST)
+           return this.userService.createUser(dto)
+       } catch (e) {
+           throw new Error(e)
+       }
     }
     async loginUser(dto: UserLoginDTO): Promise<AuthUserResponse>{
-        const existUser = await this.userService.findUserByEmail(dto.email)
-        if(!existUser) throw new BadRequestException(AppError.USER_NOT_EXIST)
-        const validatePassword = await  bcrypt.compare(dto.password, existUser.password)
-        if(!validatePassword) throw new BadRequestException(AppError.WRONG_DATA)
-      /*  const userData = {
-            name: existUser.firstName,
-            email: existUser.email
-        }*/
-        const user = await this.userService.publicUser(dto.email)
-        const token = await this.tokenService.generateJwtToken(user)
+        try {
+            const existUser = await this.userService.findUserByEmail(dto.email)
+            if(!existUser) throw new BadRequestException(AppError.USER_NOT_EXIST)
+            const validatePassword = await  bcrypt.compare(dto.password, existUser.password)
+            if(!validatePassword) throw new BadRequestException(AppError.WRONG_DATA)
+            /*  const userData = {
+                  name: existUser.firstName,
+                  email: existUser.email
+              }*/
+            const user = await this.userService.publicUser(dto.email)
+            const token = await this.tokenService.generateJwtToken(user)
 
-        return {user, token}
+            return {user, token}
+        } catch (e) {
+            throw new Error(e)
+        }
     }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -11,42 +11,70 @@ import {Watchlist} from "../watchlist/models/watchlist.model";
 export class UsersService {
     constructor(@InjectModel(User) private readonly userRepository: typeof User) {
     }
-    async hashPassword(password){
-        return bcrypt.hash(password, 10)
+    async hashPassword(password: string): Promise<string>{
+        try {
+            return bcrypt.hash(password, 10)
+        } catch (e){
+            throw new Error(e)
+        }
     }
 
-    async findUserByEmail(email: string){
-        return this.userRepository.findOne({ where: { email: email } });
+    async findUserByEmail(email: string): Promise<User>{
+        try {
+            return this.userRepository.findOne({ where: { email: email }, include: {
+                    model: Watchlist,
+                    required: false
+                } });
+        } catch (e){
+            throw new Error(e)
+        }
+
     }
    async createUser(dto): Promise<CreateUserDTO> {
-    /*    const existUser = await this.findUserByEmail(dto.email)*/
-      /* if (existUser) throw new BadRequestException(AppError.USER_EXIST)*/
-        dto.password = await this.hashPassword(dto.password)
-       await this.userRepository.create({
-            firstName: dto.firstName,
-           username: dto.username,
-           email: dto.email,
-           password: dto.password
-       })
-      /* await this.userRepository.create(newUser)*/
-       return dto
+       try {
+           /*    const existUser = await this.findUserByEmail(dto.email)*/
+           /* if (existUser) throw new BadRequestException(AppError.USER_EXIST)*/
+           dto.password = await this.hashPassword(dto.password)
+           await this.userRepository.create({
+               firstName: dto.firstName,
+               username: dto.username,
+               email: dto.email,
+               password: dto.password
+           })
+           /* await this.userRepository.create(newUser)*/
+           return dto
+       } catch (e){
+           throw new Error(e)
+       }
+
    }
-async publicUser(email:string){
+async publicUser(email:string): Promise<User>{
+    try {
         return this.userRepository.findOne({
             where:{email},
-        attributes: {exclude: ['password']},
+            attributes: {exclude: ['password']},
             include: {
                 model: Watchlist,
                 required: false
             }
         })
+    } catch (e){
+        throw new Error(e)
+    }
+
 }
 async updateUser(email: string, dto: UpdateUserDTO): Promise<UpdateUserDTO>{
-        await this.userRepository.update(dto, {where: {email}})
-    return dto
+        try{await this.userRepository.update(dto, {where: {email}})
+            return dto
+        } catch (e) {
+            throw new Error(e)
+        }
 }
 async deleteUser (email:string): Promise<boolean> {
-        await this.userRepository.destroy({where: {email}})
+    await this.userRepository.destroy({where: {email}})
     return true
-}
+    } catch (e) {
+        throw new Error(e)
+    }
+
 }
